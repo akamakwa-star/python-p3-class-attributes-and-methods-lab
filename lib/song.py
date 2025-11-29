bca55efaeb4ce93fc5b1c86ba7da19adf299f03a
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2025-11-30 01:09:17 +0300

    lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,46 @@
 class Song:
     pass
+class Song:
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Update class-level data:
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        """Add unique genres only."""
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        """Add unique artists only."""
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        """Histogram of genres."""
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        """Histogram of artists."""
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
